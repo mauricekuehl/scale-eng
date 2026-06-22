4152a798103dd60d8d0aa705d1d1000028d806cb
Maurice Kuehl <dev@mauricekuehl.com>
2026-06-22 14:41:56 +0200

    i was stupid :/

--- services/db/app.py
+++ services/db/app.py
@@ -1,11 +1,9 @@
-from asyncio import Lock
 from typing import cast
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
-lock = Lock()
 urls: dict[str, str] = {}
 url_codes: dict[str, str] = {}
 
@@ -32,31 +30,30 @@ def url_from(body: dict[str, object]) -> str:
 
 @app.get("/{code}")
 async def get_url(code: str) -> dict[str, str]:
-    async with lock:
-        if code not in urls:
-            raise HTTPException(status_code=404)
+    try:
         return {"url": urls[code]}
+    except KeyError:
+        raise HTTPException(status_code=404) from None
 
 
 # Only for testing purposes
 @app.delete("/")
 async def delete_all() -> dict[str, int]:
-    async with lock:
-        count = len(urls)
-        urls.clear()
-        url_codes.clear()
+    count = len(urls)
+    urls.clear()
+    url_codes.clear()
     return {"deleted": count}
 
 
 @app.put("/{code}")
 async def put_url(code: str, request: Request) -> JSONResponse:
     original_url = url_from(await json_body(request))
-    async with lock:
-        if original_url in url_codes:
-            return JSONResponse({"code": url_codes[original_url]})
-        if code in urls:
-            raise HTTPException(status_code=409, detail="exists")
+    existing_code = url_codes.get(original_url)
+    if existing_code is not None:
+        return JSONResponse({"code": existing_code})
+    if code in urls:
+        raise HTTPException(status_code=409, detail="exists")
 
-        urls[code] = original_url
-        url_codes[original_url] = code
-        return JSONResponse({"code": code}, status_code=201)
+    urls[code] = original_url
+    url_codes[original_url] = code
+    return JSONResponse({"code": code}, status_code=201)
